89dab8de6f33f198bcd405d9fba6cbd662f74693
Hatice Kübra Evren <haticeevren43@gmail.com>
2026-09-14 16:37:27 +0300

    Initial commit

---
+++ src/main/java/com/kubraevren/e_ticaret_project/ETicaretProjectApplicationStarter.java
@@ -0,0 +1,13 @@
+package com.kubraevren.e_ticaret_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETicaretProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ETicaretProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kubraevren/e_ticaret_project/ETicaretProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubraevren.e_ticaret_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETicaretProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
